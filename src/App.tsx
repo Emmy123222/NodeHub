4b6c80232f99c132588dd39ae96e7e6a0c75a89a
Emmanuel <emmanuelogheneovo17@gmail.com>
2025-02-09 12:36:06 +0100

    Node

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import Checkout from "./pages/Checkout";
 import Hard from "./pages/Hard";
 import ProductDetailPage from "./pages/ProductDetailPage";
 import PCBuilder from "./pages/PCBuilder";
+import Cart from "./pages/Cart";
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
           <Route path="/processor/:type" element={<Hard />} />
           <Route path="/product/:id" element={<ProductDetailPage />} />
           <Route path="/pcbuilder" element={<PCBuilder />} />
+          <Route path="/cart" element={<Cart />} />
         </Routes>
       </Layout>
     </Router>
--- src/pages/ProductDetailPage.tsx
+++ src/pages/ProductDetailPage.tsx
@@ -11,6 +11,11 @@ import PCBuilder from "./PCBuilder";
 
 function ProductDetailPage() {
   const navigate = useNavigate();
+  const handleAddToCart = () => {
+    // Navigate to the cart page
+    navigate("/cart");
+  };
+
   const [isCustomize, setIsCustomize] = useState(false);
   const { id } = useParams<{ id: string }>();
 
@@ -36,7 +41,10 @@ function ProductDetailPage() {
             </div>
 
             <div className="flex items-center gap-2">
-              <button className="flex items-center gap-2 border border-cyan-500 text-black px-4 py-2 rounded-full hover:bg-cyan-500 transition">
+              <button
+                className="flex items-center gap-2 border border-cyan-500 text-black px-4 py-2 rounded-full hover:bg-cyan-500 transition"
+                onClick={handleAddToCart}
+              >
                 Add to cart <ShoppingCart size={16} />
               </button>
               <button
@@ -50,123 +58,130 @@ function ProductDetailPage() {
         </div>
       </div>
 
-      {isCustomize? 
-      <div className=""><PCBuilder/></div> :<div className="grid grid-cols-1 lg:grid-cols-1 gap-12">
-        {/* Product Image */}
-        <motion.div
-          initial={{ opacity: 0, x: -20 }}
-          animate={{ opacity: 1, x: 0 }}
-          className="relative h-[500px] rounded-xl overflow-hidden flex justify-center"
-        >
-          <img src={speaker} alt="Product" className=" flex justify-center" />
-        </motion.div>
+      {isCustomize ? (
+        <div className="">
+          <PCBuilder />
+        </div>
+      ) : (
+        <div className="grid grid-cols-1 lg:grid-cols-1 gap-12">
+          {/* Product Image */}
+          <motion.div
+            initial={{ opacity: 0, x: -20 }}
+            animate={{ opacity: 1, x: 0 }}
+            className="relative h-[500px] rounded-xl overflow-hidden flex justify-center"
+          >
+            <img src={speaker} alt="Product" className=" flex justify-center" />
+          </motion.div>
 
-        {/* Product Info */}
-        <motion.div
-          initial={{ opacity: 0, x: 20 }}
-          animate={{ opacity: 1, x: 0 }}
-          className="flex flex-col"
-        >
-          <div className="text-[#0D1B2A] p-7">
-            <p className="text-lg mb-4">
-              This mid-tower system is powered by AMD’s Ryzen processors on a
-              micro-ATX motherboard, delivering up to 16 cores for strong
-              performance across various applications. The Fractal Meshify 2
-              Compact case offers USB, audio ports, Ethernet, and WiFi, Intel
-              i7-13700, along with support for high-end GPUs, up to 128GB RAM,
-              and multiple storage options. Efficient cooling and dust filters
-              ensure optimal performance.
-            </p>
+          {/* Product Info */}
+          <motion.div
+            initial={{ opacity: 0, x: 20 }}
+            animate={{ opacity: 1, x: 0 }}
+            className="flex flex-col"
+          >
+            <div className="text-[#0D1B2A] p-7">
+              <p className="text-lg mb-4">
+                This mid-tower system is powered by AMD’s Ryzen processors on a
+                micro-ATX motherboard, delivering up to 16 cores for strong
+                performance across various applications. The Fractal Meshify 2
+                Compact case offers USB, audio ports, Ethernet, and WiFi, Intel
+                i7-13700, along with support for high-end GPUs, up to 128GB RAM,
+                and multiple storage options. Efficient cooling and dust filters
+                ensure optimal performance.
+              </p>
 
-            <p className="text-lg font-semibold">Windows 11 Ready!</p>
+              <p className="text-lg font-semibold">Windows 11 Ready!</p>
 
-            <p className="flex items-center text-lg">
-              <span className="mr-2">🚀</span> Best For: Running validator nodes
-              (Ethereum, Solana, Bitcoin)
-            </p>
+              <p className="flex items-center text-lg">
+                <span className="mr-2">🚀</span> Best For: Running validator
+                nodes (Ethereum, Solana, Bitcoin)
+              </p>
 
-            <p className="flex items-center text-lg">
-              <span className="mr-2">✅</span> Pre-installed Software: DAppNode
-              OS
-            </p>
+              <p className="flex items-center text-lg">
+                <span className="mr-2">✅</span> Pre-installed Software:
+                DAppNode OS
+              </p>
 
-            <p className="flex items-center text-lg">
-              <span className="mr-2">⭐</span> Customer Ratings:{" "}
-              <span className="">★★★★★ (4.9/5 from 23 reviews)</span>
-            </p>
-          </div>
+              <p className="flex items-center text-lg">
+                <span className="mr-2">⭐</span> Customer Ratings:{" "}
+                <span className="">★★★★★ (4.9/5 from 23 reviews)</span>
+              </p>
+            </div>
 
-          <div className="p-7 text-black">
-            <h2 className="text-lg font-bold mb-2">
-              📌 Why Choose This Product?
-            </h2>
-            <ul className="space-y-2">
-              <li className="flex items-center">
-                <span className="text-green-500 mr-2">✅</span>
-                <span>No setup required</span>
-              </li>
-              <li className="flex items-center">
-                <span className="text-green-500 mr-2">✅</span>
-                <span>
-                  Ideal for sharing - Secure, mobile, and energy efficient
-                </span>
-              </li>
-              <li className="flex items-center">
-                <span className="text-green-500 mr-2">✅</span>
-                <span>Easy to Pay - Contact support & earn rewards</span>
-              </li>
-              <li className="flex items-center">
-                <span className="text-green-500 mr-2">✅</span>
-                <span>Future-proof - Supports major blockchain protocols</span>
-              </li>
-            </ul>
+            <div className="p-7 text-black">
+              <h2 className="text-lg font-bold mb-2">
+                📌 Why Choose This Product?
+              </h2>
+              <ul className="space-y-2">
+                <li className="flex items-center">
+                  <span className="text-green-500 mr-2">✅</span>
+                  <span>No setup required</span>
+                </li>
+                <li className="flex items-center">
+                  <span className="text-green-500 mr-2">✅</span>
+                  <span>
+                    Ideal for sharing - Secure, mobile, and energy efficient
+                  </span>
+                </li>
+                <li className="flex items-center">
+                  <span className="text-green-500 mr-2">✅</span>
+                  <span>Easy to Pay - Contact support & earn rewards</span>
+                </li>
+                <li className="flex items-center">
+                  <span className="text-green-500 mr-2">✅</span>
+                  <span>
+                    Future-proof - Supports major blockchain protocols
+                  </span>
+                </li>
+              </ul>
 
-            <h2 className="text-lg font-bold mt-4 mb-2">
-              📌 What’s in the Box?
-            </h2>
-            <ul className="space-y-2 list-disc">
-              <li className="flex items-center space-x-2">
-                <span>&#8226;</span>
-                <span>Multi-Mount Pro Hardware</span>
-              </li>
-              <li className="flex items-center space-x-2">
-                <span>&#8226;</span>
-                <span>User Manual & Thermal Guide</span>
-              </li>
-              <li className="flex items-center space-x-2">
-                <span>&#8226;</span>
-                <span>Quick Start Guide</span>
-              </li>
-            </ul>
-          </div>
-        </motion.div>
-        {/* Features Section */}
-        <div className="bg-[#D1D1D6] shadow-md rounded-lg py-4 px-6 flex flex-col md:flex-row justify-between gap-6 items-center p-5">
-          {/* Feature 1 */}
-          <div className="flex items-center gap-2 ">
-            <img src={phone} alt="" />
-            <span className="text-gray-700 text-sm">
-              24/7 Customer support via Live chat and Telegram
-            </span>
-          </div>
+              <h2 className="text-lg font-bold mt-4 mb-2">
+                📌 What’s in the Box?
+              </h2>
+              <ul className="space-y-2 list-disc">
+                <li className="flex items-center space-x-2">
+                  <span>&#8226;</span>
+                  <span>Multi-Mount Pro Hardware</span>
+                </li>
+                <li className="flex items-center space-x-2">
+                  <span>&#8226;</span>
+                  <span>User Manual & Thermal Guide</span>
+                </li>
+                <li className="flex items-center space-x-2">
+                  <span>&#8226;</span>
+                  <span>Quick Start Guide</span>
+                </li>
+              </ul>
+            </div>
+          </motion.div>
+          {/* Features Section */}
+          <div className="bg-[#D1D1D6] shadow-md rounded-lg py-4 px-6 flex flex-col md:flex-row justify-between gap-6 items-center p-5">
+            {/* Feature 1 */}
+            <div className="flex items-center gap-2 ">
+              <img src={phone} alt="" />
+              <span className="text-gray-700 text-sm">
+                24/7 Customer support via Live chat and Telegram
+              </span>
+            </div>
 
-          {/* Feature 2 */}
-          <div className="flex items-center gap-2">
-            <img src={hand} alt="" />
-            <span className="text-gray-700 text-sm">
-              1-year Manufacturer warranty
-            </span>
-          </div>
+            {/* Feature 2 */}
+            <div className="flex items-center gap-2">
+              <img src={hand} alt="" />
+              <span className="text-gray-700 text-sm">
+                1-year Manufacturer warranty
+              </span>
+            </div>
 
-          {/* Feature 3 */}
-          <div className="flex items-center gap-2">
-            <img src={Truck} alt="" />
-            <span className="text-gray-700 text-sm">
-              Free shipping on orders over 5 items
-            </span>
+            {/* Feature 3 */}
+            <div className="flex items-center gap-2">
+              <img src={Truck} alt="" />
+              <span className="text-gray-700 text-sm">
+                Free shipping on orders over 5 items
+              </span>
+            </div>
           </div>
         </div>
-      </div>}
+      )}
     </div>
   );
 }
